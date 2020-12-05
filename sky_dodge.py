366e84cc6a69dbdba5f67a73d581d8670ffb6146
Witchoutname <folyn.tomas1@gmail.com>
2020-12-05 18:21:42 +0100

    tutorial part 4

--- sky_dodge.py
+++ sky_dodge.py
@@ -11,13 +11,15 @@ class Player(pg.sprite.Sprite):
 
     def update(self, keys):
         if keys[K_UP]:
-            self.rect.move_ip(0,-1)
+            self.rect.move_ip(0, -5)
+            sounds["move_up"].play()
         if keys[K_DOWN]:
-            self.rect.move_ip(0,1)
+            self.rect.move_ip(0, 5)
+            sounds["move_down"].play()
         if keys[K_LEFT]:
-            self.rect.move_ip(-1, 0)
+            self.rect.move_ip(-5, 0)
         if keys[K_RIGHT]:
-            self.rect.move_ip(1, 0)
+            self.rect.move_ip(5, 0)
 
         if self.rect.left < 0:
             self.rect.left = 0
@@ -38,7 +40,7 @@ class Enemy(pg.sprite.Sprite):
                 SCREEN_WIDTH + 20,
                 r.randint(0, SCREEN_HEIGHT - 10)
             ])
-        self.speed = r.randint(1, 150) /100
+        self.speed = r.randint(1, 5)
 
     def update(self):
         self.rect.move_ip(-self.speed, 0)
@@ -59,13 +61,14 @@ class Cloud(pg.sprite.Sprite):
         self.speed = r.randint(1, 150) / 100
 
     def update(self):
-        self.rect.move_ip(-0.1, 0)
+        self.rect.move_ip(-5, 0)
         if self.rect.right < 0:
             self.kill()
 
 
 
 pg.init()
+clock = pg.time.Clock()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
@@ -77,9 +80,19 @@ groups["all"] = pg.sprite.Group()
 groups["all"].add(player)
 events = {}
 events["ADDENEMY"] = pg.USEREVENT + 1
-pg.time.set_timer(events["ADDENEMY"], 20)
+pg.time.set_timer(events["ADDENEMY"], 150)
 events["ADDCLOUD"] = pg.USEREVENT + 1
-pg.time.set_timer(events["ADDCLOUD"], 100)
+pg.time.set_timer(events["ADDCLOUD"], 1000)
+pg.mixer.music.load("sound/Sky_dodge_theme.ogg")
+pg.mixer.music.set_volume(0.05)
+pg.mixer.music.play(loops=-1)
+sounds = {}
+sounds["move_up"] = pg.mixer.Sound("sound/Jet_up.ogg")
+sounds["move_down"] = pg.mixer.Sound("sound/Jet_down.ogg")
+sounds["collision"] = pg.mixer.Sound("sound/Boom.ogg")
+
+for sound in sounds:
+    sounds[sound].set_volume(0.1)
 
 
 runnig = True
@@ -112,8 +125,15 @@ while runnig:
     screen.blit(player.surf, player.rect)
     if pg.sprite.spritecollideany(player, groups["enemies"]):
         player.kill()
+        sounds["move_up"].stop()
+        sounds["move_down"].stop()
+        sounds["collision"].play()
+        pg.time.delay(500)
         runnig = False
 
     pg.display.flip()
+    clock.tick(120)
 
+pg.mixer.music.stop()
+pg.mixer.quit()
 pg.quit()
